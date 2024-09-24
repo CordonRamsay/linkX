eb8ca3dce192446f0174fd560c00a6a135fd7971
Hyunhopark1 <phh1569@nate.com>
2024-09-24 18:01:48 +0900

    초기세팅

---
+++ linkx/src/main/java/com/mjc/linkx/LinkxApplication.java
@@ -0,0 +1,13 @@
+package com.mjc.linkx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkxApplication.class, args);
+    }
+
+}
---
+++ linkx/src/test/java/com/mjc/linkx/LinkxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjc.linkx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
