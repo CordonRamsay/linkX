46325c7f27923e1596b1f28c1d10d5c6b2978d8c
Hyunhopark1 <132931453+Hyunhopark1@users.noreply.github.com>
2024-10-16 13:03:42 +0900

    add index.html

--- linkx/src/main/java/com/mjc/linkx/common/IndexController.java
+++ linkx/src/main/java/com/mjc/linkx/common/IndexController.java
@@ -1,13 +1,27 @@
-package com.mjc.linkx.boardfree;
+package com.mjc.linkx.common;
 
 
-import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/boardFree")
-public class BoardFreeController {
+@RequestMapping("/index")
+public class IndexController {
 
+    @GetMapping("/")
+    public String index(){
+
+
+
+
+
+
+
+
+
+
+
+        return "index";
+    }
 }
