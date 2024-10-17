90d550e8fc5743671330108e560ac9920c141647
Hyunhopark1 <phh1569@nate.com>
2024-10-17 22:27:52 +0900

    BoardDeptDto 완성

--- linkx/src/main/java/com/mjc/linkx/boarddept/BoardDeptDto.java
+++ linkx/src/main/java/com/mjc/linkx/boarddept/BoardDeptDto.java
@@ -2,7 +2,17 @@ package com.mjc.linkx.boarddept;
 
 import com.mjc.linkx.boardcommon.BoardBaseDto;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardDeptDto extends BoardBaseDto implements IBoardDept {
 
     private Long id;
--- linkx/src/main/java/com/mjc/linkx/boarddept/IBoardDept.java
+++ linkx/src/main/java/com/mjc/linkx/boarddept/IBoardDept.java
@@ -3,5 +3,21 @@ package com.mjc.linkx.boarddept;
 import com.mjc.linkx.boardcommon.IBoardBase;
 
 public interface IBoardDept extends IBoardBase {
+    Long getId();
+    void setId(Long id);
 
+    String getTitle();
+    void setTitle(String title);
+
+    String getContent();
+    void setContent(String content);
+
+    Integer getViewQty();
+    void setViewQty(Integer viewQty);
+
+    Integer getLikeQty();
+    void setLikeQty(Integer likeQty);
+
+    Long getMajorId();
+    void setMajorId(Long majorId);
 }
