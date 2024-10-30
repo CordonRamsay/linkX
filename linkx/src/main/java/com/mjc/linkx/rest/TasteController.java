0d6497da8e6a8aec92b86e9a9651c88d44358f70
psh9758 <p_sh9758@naver.com>
2024-10-30 18:35:25 +0900

    taste

--- linkx/src/main/java/com/mjc/linkx/rest/TasteController.java
+++ linkx/src/main/java/com/mjc/linkx/rest/TasteController.java
@@ -1,14 +1,16 @@
 package com.mjc.linkx.rest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
-@RequestMapping("/rest")
-public class RestController {
+@RequestMapping("/taste")
+public class TasteController {
     @GetMapping("")
-    public String rest(){
-        return "rest";
+    public String taste() {
+        return "taste";
     }
 }
