3d8a08acde42811c7f76bd314436daf74f42d22d
Hyunhopark1 <132931453+Hyunhopark1@users.noreply.github.com>
2024-11-06 18:29:18 +0900

    학과게시판 좋아요 진행중

--- linkx/src/main/java/com/mjc/linkx/boarddept/BoardDeptDto.java
+++ linkx/src/main/java/com/mjc/linkx/boarddept/BoardDeptDto.java
@@ -1,7 +1,7 @@
 package com.mjc.linkx.boarddept;
 
 import com.mjc.linkx.boardcommon.BoardBaseDto;
-import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,10 +18,10 @@ public class BoardDeptDto extends BoardBaseDto implements IBoardDept {
     // id
     private Long id;
     //  제목
-    @Size(min = 2, max = 100, message = "제목은 2자~100자 사이로 입력해 주세요.")
+//    @Size(min = 2, max = 100, message = "제목은 2자~100자 사이로 입력해 주세요.")
     private String title;
     //  내용
-    @Size(min = 2, max = 1000, message = "본문은 2자~1000자 사이로 입력해 주세요.")
+//    @Size(min = 2, max = 1000, message = "본문은 2자~1000자 사이로 입력해 주세요.")
     private String content;
     //조회수
     private Integer viewQty;
--- linkx/src/main/java/com/mjc/linkx/boardfree/BoardFreeDto.java
+++ linkx/src/main/java/com/mjc/linkx/boardfree/BoardFreeDto.java
@@ -1,7 +1,7 @@
 package com.mjc.linkx.boardfree;
 
 import com.mjc.linkx.boardcommon.BoardBaseDto;
-import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,11 +17,11 @@ public class BoardFreeDto extends BoardBaseDto implements IBoardFree {
 
     // id
     private Long id;
-    @Size(min = 2, max = 100, message = "제목은 2자~100자 사이로 입력해 주세요.")
+//    @Size(min = 2, max = 100, message = "제목은 2자~100자 사이로 입력해 주세요.")
 
     // 제목
     private String title;
-    @Size(min = 2, max = 1000, message = "본문은 2자~1000자 사이로 입력해 주세요.")
+//    @Size(min = 2, max = 1000, message = "본문은 2자~1000자 사이로 입력해 주세요.")
 
     // 내용
     private String content;
