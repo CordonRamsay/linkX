708b5e42c82440a69e78008613fae32fc2cdfb2f
shyunho <shyunho0223@naver.com>
2024-11-15 13:29:56 +0900

    치명적인 버그로 인해 동의자 수 증가 전으로 다시 되돌리기

--- linkx/src/main/java/com/mjc/linkx/petition/PetitionServiceImpl.java
+++ linkx/src/main/java/com/mjc/linkx/petition/PetitionServiceImpl.java
@@ -5,8 +5,6 @@ import com.mjc.linkx.user.IUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -15,7 +13,6 @@ public class PetitionServiceImpl implements IPetitionService{
 
     //청원 게시판 mapper 객체 변수 선언(생성자 주입)
     private final IPetitionMyBatisMapper petitionMyBatisMapper;
-    private final IPetiSignitureMapper petiSignitureMapper;
 
     //원본 코드를 따라서 작성하다가 현재 insert만 따라 하고 나머지는 html작성 후 테스트 진행후 다시 코드 따라하기로 함
     @Override
@@ -63,27 +60,6 @@ public class PetitionServiceImpl implements IPetitionService{
 
     @Override
     public void addagreeQty(Long id, IUser user) {
-        if(id == null || id < 0 || user == null){
-            return;
-        }
 
-        PetiSignitureDto psdto = PetiSignitureDto.builder()
-                .userId(user.getId())
-                .petiId(id)
-                .sigDt(String.valueOf(LocalDateTime.now()))
-                .build();
-
-        Integer count = this.petiSignitureMapper.countByIdAndUser(psdto);
-        if(count > 0){
-            return;
-        }
-
-        //서명 테이블에 행 삽입
-        this.petiSignitureMapper.insert(psdto);
-
-        //청원게시판 테이블에 동의자 수 증가
-        this.petitionMyBatisMapper.addAgreeQty(id);
     }
-
-
 }
