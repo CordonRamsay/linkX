d4f36d36482f9a47902870a1ebb3adec709dfa50
psh9758 <p_sh9758@naver.com>
2024-11-16 18:53:16 +0900

    review 구현

--- linkx/src/main/java/com/mjc/linkx/taste/TasteService.java
+++ linkx/src/main/java/com/mjc/linkx/taste/TasteService.java
@@ -6,4 +6,6 @@ import java.util.List;
 public interface TasteService {
     String fetchAndSaveTasteData();
     List<TasteRestDto> getTasteList();
+    List<TasteReviewDto> getReviewsByRestaurantId(Long restId);
+    void addReview(TasteReviewDto reviewDto);
 }
